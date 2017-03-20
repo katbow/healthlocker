224774c67175c57f5a4b4b7c178899dcad270232
Cleop <cleo.pearson@hotmail.co.uk>
2017-03-20 16:13:16 +0000

    Amends test so that 3rd a tag is being tested due to new code making old test fail

--- test/acceptance_tests/menu_test.js
+++ test/acceptance_tests/menu_test.js
@@ -1,34 +1,19 @@
 const Nightmare = require('nightmare')
 const nightmare = Nightmare({ show: true })
 
-
-// Tests that "Home" is the innerHTML for the second a tag on the menu page
-nightmare.goto('https://www.healthlocker.uk/')
-  .click('i#open-nav')
-  .screenshot('./test/acceptance_tests/screenshots/menu.png')
-  .evaluate(function(){
-    return document.getElementsByTagName('a')[1].innerHTML;
-  })
-  .end()
-    .then(function (result) {
-      result == "Home" ? console.log(`Test passed: ${result} received`) : console.log(`${result} received, expected "Home"`);
-    })
-    .catch(function (error) {
-      console.error('Error:', error);
-    });
-
-    // nightmare.goto('https://www.healthlocker.uk/')
-    //   .click('i#open-nav')
-    //   .on('did-finish-load', function(){
-    //     return nightmare.screenshot('./test/acceptance_tests/screenshots/menu.png')
-    //   })
-    //   .evaluate(function(){
-    //     return document.getElementsByTagName('a')[1].innerHTML;
-    //   })
-    //   .end()
-    //     .then(function (result) {
-    //       result == "Home" ? console.log(`Test passed: ${result} received`) : console.log(`${result} received, expected "Home"`);
-    //     })
-    //     .catch(function (error) {
-    //       console.error('Error:', error);
-    //     });
+// Tests that "Home" is the innerHTML for the third 'a' tag on the menu page
+nightmare
+.goto('https://www.healthlocker.uk/')
+.click('i#open-nav')
+.wait(1000) // https://github.com/dwyl/learn-daydream-and-nightmare/issues/5
+.screenshot('./test/acceptance_tests/screenshots/menu_test.png')
+.evaluate(function(){
+  return document.getElementsByTagName('a')[2].innerHTML;
+})
+.end()
+.then(function (result) {
+  result == "Home" ? console.log(`Test passed: ${result} received`) : console.log(`Test failed: ${result} received, expected "Home"`);
+})
+.catch(function (error) {
+  console.error('Menu Test Error:', error);
+});
