15a6d8e881b5644a9bec40f917b0f4e080810c60
robstallion <robertefrancis18@gmail.com>
2017-06-22 20:45:52 +0100

    Use concache for EPJS user data #858

--- web/controllers/caseload_controller.ex
+++ web/controllers/caseload_controller.ex
@@ -5,7 +5,7 @@ defmodule Healthlocker.CaseloadController do
 
   def index(conn, _params) do
     clinician = conn.assigns.current_user
-    patient_ids = ConCache.get_or_store(:hl_cache, :team_members, fn () ->
+    patient_ids = ConCache.get_or_store(:hl_cache, :patient_ids, fn () ->
       EPJSTeamMember
                   |> EPJSTeamMember.patient_ids(clinician."Staff_ID")
                   |> ReadOnlyRepo.all
@@ -20,17 +20,21 @@ defmodule Healthlocker.CaseloadController do
               end)
               |> Enum.concat
 
-    non_hl = patient_ids
-              |> Enum.map(fn id ->
-                ReadOnlyRepo.all(from e in EPJSUser,
-                where: e."Patient_ID" == ^id)
-              end)
-              |> Enum.concat
-              |> Enum.reject(fn user ->
-                Enum.any?(hl_users, fn hl ->
-                  user."Patient_ID" == hl.slam_id
-                end)
-              end)
+
+    non_hl = ConCache.get_or_store(:hl_cache, :non_hl, fn () ->
+      patient_ids
+      |> Enum.map(fn id ->
+        ReadOnlyRepo.all(from e in EPJSUser,
+        where: e."Patient_ID" == ^id)
+      end)
+      |> Enum.concat
+      |> Enum.reject(fn user ->
+        Enum.any?(hl_users, fn hl ->
+          user."Patient_ID" == hl.slam_id
+        end)
+      end)
+    end)
+    
     render(conn, "index.html", hl_users: hl_users, non_hl: non_hl)
   end
 end
