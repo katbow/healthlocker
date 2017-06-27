ccb0bdfe403e508a4dc0fc1b75b4e6be93dc2fbe
robstallion <robertefrancis18@gmail.com>
2017-06-27 16:03:37 +0100

    add code to test decoding string with elixir

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -3,6 +3,28 @@ defmodule Healthlocker.PageController do
   alias Healthlocker.Post
 
   def index(conn, _params) do
+    str = "AQAAADyhSQUqcHkOWTRX80nd0Qfa7rO+x63hsOMqs2UzJd60kzxrYZb8C9Klf4SYQjX1BOV1WFXuDd2xXd36niOxuo67K8iyrnGqqeZgkGE6AOMTeGyHkoY7QoGAagl17DsXS07FhlY8iAIdD4lb2eAYMXd6tXsV4+HSjlJ0ufKL7lCU";
+    query =
+      """
+      DECLARE @UserData VARBINARY(MAX), @passphrase varchar(255), @UserDataString VARCHAR(MAX)--, @userName varchar(255);
+      SET @passphrase = HASHBYTES('MD5', N'pass');
+      SET @UserDataString = '${str}'
+      -- SET @UserDataString =
+      SELECT @UserData =
+              CAST(N'' AS XML).value(
+                  'xs:base64Binary(sql:variable("@UserDataString"))'
+                , 'VARBINARY(MAX)'
+              )
+      ;
+      SELECT CONVERT(VARCHAR(8000),DecryptByPassPhrase(@passphrase,@UserData,1,CONVERT(varbinary, 15220)));
+      SELECT CONVERT(VARCHAR(8000),DecryptByPassPhrase(@passphrase,@UserData));
+      """
+
+      query2 = "SELECT top 10 * from mhlTeamMembers"
+
+    SQL.query(ReadOnlyRepo, query, [])
+    |> IO.inspect(label: "decoded string")
+
     featured_story = Post
                      |> Post.find_stories
                      |> Repo.all
